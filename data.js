c65e103a7ee579ea14c6a886dd33b9567b07ad39
Isabela Cortes B <isabelacortesb@MacBook-Pro-de-Isabela.local>
2022-09-02 21:50:03 -0500

    JSON Isabela c

---
+++ data.js
@@ -0,0 +1,145 @@
+const data = [
+    
+    {
+        "name" : "Andres",
+        "views"  : "2.000",
+        "comments": "27",
+        "day" : "2 DAYS AGO",
+        "infopublicacion" : "Just two friends having a good time",
+        "hashtag" : "#photo",
+        "imagenpost" : "./img/usuarioPost1.jpeg",
+        "imagenusuario" : "./img/usuario1.jpeg",
+        "ubicacion" : "Madrid"
+
+
+    },
+
+
+    {
+        "name" : "Viviana",
+        "views"  : "3.000",
+        "comments": "47",
+        "day" : "7 DAYS AGO",
+        "infopublicacion" : "Today’s been the best day",
+        "hashtag" : "#post",
+        "imagenpost" : "./img/usuarioPost2.jpeg",
+        "imagenusuario" : "./img/usuario2.jpeg",
+        "ubicacion" : "Paris"
+
+
+    },
+
+    {
+        "name" : "Sara",
+        "views"  : "5.000",
+        "comments": "98",
+        "day" : "9 DAYS AGO",
+        "infopublicacion" : "Time for an adventure!",
+        "hashtag" : "#instagram",
+        "imagenpost" : "./img/usuarioPost3.jpg",
+        "imagenusuario" : "./img/usuario3.jpg",
+        "ubicacion" : "Spain"
+
+
+    },
+
+    {
+        "name" : "Santiago",
+        "views"  : "900",
+        "comments": "108",
+        "day" : "10 DAYS AGO",
+        "infopublicacion" : "Good food, good mood",
+        "hashtag" : "#photo",
+        "imagenpost" : "./img/usuarioPost4.jpg",
+        "imagenusuario" : "./img/usuario4.jpg",
+        "ubicacion" : "Afghanistan"
+
+
+    },
+
+    {
+        "name" : "Matias",
+        "views"  : "500",
+        "comments": "100",
+        "day" : "2 DAYS AGO",
+        "infopublicacion" : "Life is better when you are laughing",
+        "hashtag" : "#food",
+        "imagenpost" : "./img/usuarioPost5.jpg",
+        "imagenusuario" : "./img/usuario5.jpg",
+        "ubicacion" : "	Austria"
+
+
+    },
+
+    {
+        "name" : "Daniela",
+        "views"  : "950",
+        "comments": "38",
+        "day" : "4 DAYS AGO",
+        "infopublicacion" : "Selfie Sunday",
+        "hashtag" : "#amazing",
+        "imagenpost" : "./img/usuarioPost6.jpg",
+        "imagenusuario" : "./img/usuario6.jpg",
+        "ubicacion" : "Belgium"
+
+
+    },
+
+    {
+        "name" : "Tatiana",
+        "views"  : "500",
+        "comments": "200",
+        "day" : "4 DAYS AGO",
+        "infopublicacion" : "Couldnt be happier",
+        "hashtag" : "#beach",
+        "imagenpost" : "./img/usuarioPost7.jpg",
+        "imagenusuario" : "./img/usuario7.jpg",
+        "ubicacion" : "Chile"
+
+
+    },
+
+    {
+        "name" : "Maria Antonia",
+        "views"  : "1.200",
+        "comments": "408",
+        "day" : "6 DAYS AGO",
+        "infopublicacion" : "Just the two of us",
+        "hashtag" : "#sunset",
+        "imagenpost" : "./img/usuarioPost8.jpg",
+        "imagenusuario" : "./img/usuario8.jpg",
+        "ubicacion" : "	Egypt"
+
+
+    },
+
+    {
+        "name" : "Juan Felipe",
+        "views"  : "234",
+        "comments": "108",
+        "day" : "12 DAYS AGO",
+        "infopublicacion" : "We did it!",
+        "hashtag" : "#life",
+        "imagenpost" : "./img/usuarioPost9.jpg",
+        "imagenusuario" : "./img/usuario9.jpg",
+        "ubicacion" : "Germany"
+
+
+    },
+
+    {
+        "name" : "Esteban",
+        "views"  : "680",
+        "comments": "158",
+        "day" : "5 DAYS AGO",
+        "infopublicacion" : "A walk in the woods feels like home",
+        "hashtag" : "#fitness",
+        "imagenpost" : "./img/usuarioPost10.jpg",
+        "imagenusuario" : "./img/usuario10.jpg",
+        "ubicacion" : "	Iceland"
+
+
+    }
+]
+
+export default data; 
